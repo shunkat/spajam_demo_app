54d868f9c8140fc38449c25e06a3af13539082d7
shunkat <shunkat@icloud.com>
2022-09-03 19:04:35 +0900

    見た目完成

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:spajam_demo_app/src/register/register.dart';
 
 import 'sample_feature/sample_item_details_view.dart';
 import 'sample_feature/sample_item_list_view.dart';
@@ -73,7 +74,7 @@ class MyApp extends StatelessWidget {
                     return const SampleItemDetailsView();
                   case SampleItemListView.routeName:
                   default:
-                    return const SampleItemListView();
+                    return const RegisterView();
                 }
               },
             );
---
+++ lib/src/register/register.dart
@@ -0,0 +1,95 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(RegisterView());
+}
+
+class RegisterView extends StatelessWidget {
+  const RegisterView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('FlutterZero'),
+        ),
+        body: MyRegisterView(),
+      ),
+    );
+  }
+}
+
+class MyRegisterView extends StatefulWidget {
+  const MyRegisterView({Key? key}) : super(key: key);
+
+  @override
+  State<MyRegisterView> createState() => _MyRegisterViewState();
+}
+
+class _MyRegisterViewState extends State<MyRegisterView> {
+  String? email;
+  String? password;
+
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        children: [
+          TextField(
+            controller: _emailController,
+            onChanged: (email) {
+              this.email = email;
+            },
+            decoration: InputDecoration(hintText: 'Email'),
+          ),
+          TextField(
+            controller: _passwordController,
+            onChanged: (password) {
+              this.password = password;
+            },
+            obscureText: true,
+            decoration: InputDecoration(hintText: 'Password'),
+          ),
+          ElevatedButton(
+            child: Text('Sign In'),
+            onPressed: (){},
+
+          //  ビルド通ったら下を有効化
+          //     ElevatedButton(
+          //       child: Text('Sign In'),
+          //       onPressed: () async {
+          //         try {
+          //           await FirebaseAuth.instance.signInWithEmailAndPassword(
+          //             email: _emailController.text.trim(),
+          //             password: _passwordController.text.trim(),
+          //           );
+          //           final user = FirebaseAuth.instance.currentUser!;
+          //
+          //           final snackBar = SnackBar(
+          //             content: Text(user.email!),
+          //           );
+          //
+          //           ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          //         } catch (e) {
+          //           print(e);
+          //         }
+          //       },
+          //     )
+
+
+          )
+        ],
+      ),
+    );
+  }
+}
